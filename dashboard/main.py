d0d864fa69980d4d1fb4de05261995e7838f29d2
Pathum Danthanarayana <pathumkavindya@gmail.com>
2023-06-25 22:08:08 -0400

    GPS speed, location functionality
    - Added functionality to retrieve suburb from GPS packet and display it as location
    - Added functionality to retrieve speed from GPS packet and display it as the current speed

---
+++ dashboard/main.py
@@ -0,0 +1,27 @@
+from PyQt5 import QtWidgets, QtCore
+from dashboard import Ui_MainWindow
+from threading import Thread
+import time
+import sys
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        # Initialize GUI
+        self.dashboard = Ui_MainWindow()
+        self.dashboard.initialize_gps()
+        self.dashboard.setupUi(self)
+        self.dashboard.play_startup_sound()
+    
+    def closeEvent(self, event):
+        self.dashboard.stop()
+
+def main():
+    app = QtWidgets.QApplication([])
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
