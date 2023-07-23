73981212edcdc9be3c3c4f6f8a1c33547a5ec2d3
Pasindu Danthanarayana <danthe.pasindu@gmail.com>
2023-07-23 18:14:05 -0400

    Added tab for rear camera and collision detection
    Created signal-slot connection for first ultrasonic sensor and mechanism for collision detection. Added picamera cv2 video feed in the background with parking guidance.

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,20 +1,60 @@
 import cv2
 import csv
+import os
+from gpiozero.pins.pigpio import PiGPIOPin
 from imutils.video import VideoStream
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import QTimer
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, QThread
+from PyQt5.QtGui import QPixmap, QImage
 import gpsd
-import vlc
 from threading import Thread
 import time
 from datetime import datetime
 import subprocess
 from gpiozero import DistanceSensor 
 from time import sleep
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3
+from mutagen import File
+from PIL import Image
+from io import BytesIO
+import vlc
+import random
+
+
+class SensorThread(QThread):
+    SensorUpdate = pyqtSignal(float)
+    
+    def run(self):
+        sensor = DistanceSensor(24, 23)
+
+        self.ThreadActive = True
+
+        while self.ThreadActive:
+            total_distance = round(sensor.distance*100)
+            self.SensorUpdate.emit(total_distance)
+
+class VideoThread(QThread):
+    ImageUpdate = pyqtSignal(QImage)
+
+    def run(self):
+        self.ThreadActive = True
+        usingPiCamera = True
+        frameSize = (1024, 600)
+        vs = VideoStream(src=0, usePiCamera=usingPiCamera, resolution=frameSize, framerate=32).start()
+        time.sleep(0.5)
+        while self.ThreadActive:     
+            self.frame = vs.read()  
+            self.image = QtGui.QImage(self.frame.data, self.frame.shape[1], self.frame.shape[0], QtGui.QImage.Format_RGB888).rgbSwapped()
+            time.sleep(0.1)
+            self.ImageUpdate.emit(self.image)
+        cv2.destroyAllWindows()  
+        vs.stop()
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
+        
+        self.folder_path = os.path.dirname(os.path.abspath(__file__))
 
         self.setup_fonts()
 
@@ -29,7 +69,7 @@ class Ui_MainWindow(object):
         self.initialize_ui(MainWindow)
 
         # Launch the GUI with Tab 1 selected
-        self.tabWidget.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(2)
         self.play_startup_sound()
 
     def setup_fonts(self):
@@ -38,6 +78,7 @@ class Ui_MainWindow(object):
         QtGui.QFontDatabase.addApplicationFont("fonts/Ridge-Bold-Oblique.otf")
         
         self.light_30_font = QtGui.QFont("RIDGE-LIGHT-OBLIQUE", 30)
+        self.bold_30_font = QtGui.QFont("Ridge-Bold-Oblique", 30)
         self.light_20_font = QtGui.QFont("RIDGE-LIGHT-OBLIQUE", 20)
         self.speed_font = QtGui.QFont("Ridge-Bold-Oblique", 120)
         
@@ -123,7 +164,7 @@ class Ui_MainWindow(object):
         self.kmhLabel.setAlignment(QtCore.Qt.AlignCenter)
         self.kmhLabel.setObjectName("kmhLabel")
         self.timeLabel = QtWidgets.QLabel(self.mainDash)
-        self.timeLabel.setGeometry(QtCore.QRect(410, 0, 211, 71))
+        self.timeLabel.setGeometry(QtCore.QRect(390, 0, 261, 71))
         
         # Time label
         self.timeLabel.setFont(self.light_30_font)
@@ -270,27 +311,6 @@ class Ui_MainWindow(object):
         self.front_3d_OFF = QPixmap("graphics/front_3d_OFF.png")
 
 
-        self.vs = VideoStream(src=0, usePiCamera=True, resolution=(1024, 600), framerate=32).start()
-
-
-
-        self.sensor = DistanceSensor(24, 23)
-        min_dist = 2
-        max_dist = 100
-        self.dist_step_01 = min_dist + round((max_dist - min_dist) / 4 * 1)
-        self.dist_step_02 = min_dist + round((max_dist - min_dist) / 4 * 2)#0.1425
-        self.dist_step_03 = min_dist + round((max_dist - min_dist) / 4 * 3)
-        self.dist_step_04 = min_dist + round((max_dist - min_dist) / 4 * 4)
-        print(self.dist_step_01)
-
-        self.timer1=QTimer()
-        self.timer1.timeout.connect(self.update_camera)
-        self.timer1.start()
-
-        self.timer2=QTimer()
-        self.timer2.timeout.connect(self.update_sensors)
-        self.timer2.start()
-
 
         self.overlayLabel.setPixmap(self.overlay)
         self.centerLabel.setPixmap(self.center)
@@ -318,9 +338,190 @@ class Ui_MainWindow(object):
         self.cameraView.setLayout(vboxTab1)
         self.tabWidget.addTab(self.cameraView, "")
 
-    def setup_media_tab(self, MainWindow):
-        pass
+        self.min_dist = 2
+        self.max_dist = 100
+        self.dist_step_01 = self.min_dist + round((self.max_dist - self.min_dist) / 4 * 1)
+        self.dist_step_02 = self.min_dist + round((self.max_dist - self.min_dist) / 4 * 2)#0.1425
+        self.dist_step_03 = self.min_dist + round((self.max_dist - self.min_dist) / 4 * 3)
+        self.dist_step_04 = self.min_dist + round((self.max_dist - self.min_dist) / 4 * 4)
+        
+        self.VideoThread = VideoThread()
+        self.VideoThread.start()
+        self.VideoThread.ImageUpdate.connect(self.ImageUpdateSlot)
 
+        self.SensorThread = SensorThread()
+        self.SensorThread.start()
+        self.SensorThread.SensorUpdate.connect(self.SensorUpdateSlot)
+    
+    def ImageUpdateSlot(self, Image):
+        self.bgLabel.setPixmap(QPixmap.fromImage(Image))
+
+    def SensorUpdateSlot(self, distance):
+        self.front_1a_Label.setPixmap(self.front_1a_ON if distance < self.dist_step_01 else self.front_1a_OFF)
+        self.front_1b_Label.setPixmap(self.front_1b_ON if distance < self.dist_step_02 else self.front_1b_OFF)
+        self.front_1c_Label.setPixmap(self.front_1c_ON if distance < self.dist_step_03 else self.front_1c_OFF)
+        self.front_1d_Label.setPixmap(self.front_1d_ON if distance < self.dist_step_04 else self.front_1d_OFF)  
+
+    
+    def closeEvent(self, event):
+        self.thread.stop()
+        event.accept()
+    '''
+    @pyqtSlot(QtCore.QModelIndex)
+    def update_image(self, cv_img):
+        self.bgLabel.setPixmap(QPixmap.fromImage(cv_img))
+    '''
+
+    def setup_media_tab(self, MainWindow):
+        # Setting up Tab 3: Media player
+        self.mediaTab = QtWidgets.QWidget()
+        self.mediaTab.setObjectName("mediaTab")
+        self.tabWidget.addTab(self.mediaTab, "")
+
+        self.albumCover = QtWidgets.QLabel(self.mediaTab)
+        self.albumCover.setGeometry(QtCore.QRect(370, 60, 300, 300))
+        self.albumCover.setText("")
+        self.albumCover.setObjectName("albumCover")
+
+        self.trackName = QtWidgets.QLabel(self.mediaTab)
+        self.trackName.setGeometry(QtCore.QRect(120, 380, 800, 20))
+        self.trackName.setAlignment(QtCore.Qt.AlignCenter)
+        self.trackName.setObjectName("trackName")
+        self.trackName.setText("Track Name")
+        self.trackName.setFont(self.bold_30_font)
+        self.trackName.setStyleSheet("color: #01CBEE")
+
+        self.artistName = QtWidgets.QLabel(self.mediaTab)
+        self.artistName.setGeometry(QtCore.QRect(120, 420, 800, 20))
+        self.artistName.setAlignment(QtCore.Qt.AlignCenter)
+        self.artistName.setObjectName("artistName")
+        self.artistName.setText("Artist Name")
+        self.artistName.setFont(self.light_20_font)
+        self.artistName.setStyleSheet("color: white")
+
+        self.playPauseButton = QtWidgets.QPushButton(self.mediaTab)
+        self.playPauseButton.setGeometry(QtCore.QRect(410, 490, 215, 23))
+        self.playPauseButton.setObjectName("playPauseButton")
+        self.playPauseButton.setText("Play/Pause")
+        self.playPauseButton.setFont(self.light_20_font)
+        self.playPauseButton.setStyleSheet("color: #01CBEE")
+        self.playPauseButton.clicked.connect(self.play_track)
+
+        self.nextButton = QtWidgets.QPushButton(self.mediaTab)
+        self.nextButton.setGeometry(QtCore.QRect(700, 490, 100, 23))
+        self.nextButton.setObjectName("nextButton")
+        self.nextButton.setText("Next")
+        self.nextButton.setFont(self.light_20_font)
+        self.nextButton.setStyleSheet("color: #01CBEE")
+        self.nextButton.clicked.connect(self.next_track)
+
+        self.prevButton = QtWidgets.QPushButton(self.mediaTab)
+        self.prevButton.setGeometry(QtCore.QRect(200, 490, 100, 23))
+        self.prevButton.setObjectName("prevButton")
+        self.prevButton.setText("Prev")
+        self.prevButton.setFont(self.light_20_font)
+        self.prevButton.setStyleSheet("color: #01CBEE")
+        self.prevButton.clicked.connect(self.prev_track)
+
+        """
+        self.horizontalSlider = QtWidgets.QSlider(self.mediaTab)
+        self.horizontalSlider.setGeometry(QtCore.QRect(219, 280, 221, 20))
+        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider.setObjectName("horizontalSlider")
+        """
+
+        self.timeLabel_2 = QtWidgets.QLabel(self.mediaTab)
+        self.timeLabel_2.setGeometry(QtCore.QRect(390, 0, 261, 71))
+        self.timeLabel_2.setFont(self.light_30_font)
+        self.timeLabel_2.setStyleSheet("color: rgb(255, 255, 255);")
+        self.timeLabel_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.timeLabel_2.setObjectName("timeLabel_2")
+
+        # Prepare media player
+        self.song_list = os.listdir(f"{self.folder_path}/music")
+        random.shuffle(self.song_list)
+        self.play_music = True
+        self.previous_song = False
+        self.next_song = False
+        self.curr_song_index = 0
+        self.song_index = 0
+        self.stop_media_player = False
+
+        self.media_player_thread = Thread(target=self.run_media_player)
+        self.media_player_thread.start()
+
+
+    
+    def run_media_player(self):
+        self.player = vlc.MediaPlayer()
+        while not self.stop_media_player:
+            # Reached past last song
+            if self.curr_song_index == len(self.song_list):
+                self.curr_song_index = 0
+            # Reached behind first song
+            elif self.curr_song_index == -1:
+                self.curr_song_index = len(self.song_list) -1
+
+            print(f"Playing {self.song_list[self.curr_song_index]}")
+            # Update track info
+            self.load_track_data(self.song_list[self.curr_song_index])
+            media = vlc.Media(f"{self.folder_path}/music/{self.song_list[self.curr_song_index]}")
+            self.player.set_media(media)
+            self.player.play()
+            # Wait for the song to play
+            time.sleep(3)
+            while True:
+                # Song is finished, move to next song
+                if self.player.is_playing() != 1 and self.play_music:
+                    self.curr_song_index += 1
+                    break
+                # Song is still playing, and next button was pressed
+                elif self.player.is_playing() and self.next_song:
+                    self.curr_song_index += 1
+                    self.next_song = False
+                    break
+                # Song is still playing, and previous button was pressed
+                elif self.player.is_playing() and self.previous_song:
+                    self.curr_song_index -= 1
+                    self.previous_song = False
+                    break
+                else:
+                    # Song is still playing
+                    time.sleep(1)
+    
+    def load_track_data(self, song):
+        track_path = f"{self.folder_path}/music/{song}"
+        print(f"Track path: {track_path}")
+        mp3_file = File(track_path, easy=True)
+        track_name = mp3_file.get('title')[0].replace("&", "and")
+        artist_name = mp3_file.get('artist')[0].replace("&", "and")
+        self.trackName.setText(f"{track_name[:25]}...")
+        self.artistName.setText(artist_name.replace("/", ", "))
+
+        tags = ID3(track_path)
+        album_cover = tags.getall("APIC")[0].data
+        qImg = QtGui.QImage()
+        qImg.loadFromData(album_cover)
+        pixmap01 = QtGui.QPixmap.fromImage(qImg)
+        pixmap_image = QtGui.QPixmap(pixmap01)
+        scaled_res = QtCore.QSize(250, 250)
+        pixmap_image = pixmap_image.scaled(scaled_res, aspectRatioMode=QtCore.Qt.KeepAspectRatio, transformMode=QtCore.Qt.SmoothTransformation)
+        self.albumCover.setPixmap(pixmap_image)
+        self.albumCover.setAlignment(QtCore.Qt.AlignCenter)
+
+    def play_track(self):
+        if self.player.is_playing():
+            self.play_music = False
+            self.player.pause()
+        else:
+            self.play_music = True
+            self.player.play()
+    
+    def prev_track(self):
+        self.previous_song = True
+    def next_track(self):
+        self.next_song = True
+    
     def initialize_ui(self, MainWindow):
         """
         _translate = QtCore.QCoreApplication.translate
@@ -360,8 +561,11 @@ class Ui_MainWindow(object):
         self.time_thread.start()
 
     def play_startup_sound(self):
-        player = vlc.MediaPlayer("sounds/startup_chime.mp3")
-        player.play()
+        self.media_player = vlc.MediaPlayer()
+        media = vlc.Media(f"{self.folder_path}/sounds/startup_chime.mp3")
+        self.media_player.set_media(media)
+        self.media_player.play()
+
 
     def initialize_gps(self):
         print("Initializing GPS...")
@@ -393,10 +597,8 @@ class Ui_MainWindow(object):
         return f"{hour}:{minute} {'PM' if int(hour) > 11 else 'AM'}"
         """
         now = datetime.now()
-        military_hour = now.strftime("%H")
-        hour = (int(military_hour) - 2) % 10
-        minute = now.strftime("%M")
-        return f"{hour}:{minute} {'PM' if int(military_hour) > 11 else 'AM'}"
+        curr_time = time.strftime("%I:%M %p")
+        return curr_time
 
     
     def get_speed(self):
@@ -406,7 +608,7 @@ class Ui_MainWindow(object):
         speed_mph = float(gps_packet.hspeed)
         # Convert speed to int and kmh
         speed_kmh_int = int(speed_mph * 1.609344)
-        print(f"Current speed: {speed_kmh_int}")
+        #print(f"Current speed: {speed_kmh_int}")
         return speed_kmh_int
     
     def get_current_suberb(self):
@@ -415,7 +617,7 @@ class Ui_MainWindow(object):
         # Get latitude and longitude
         lat = gps_packet.lat
         long = gps_packet.lon
-        print(f"My coordinates: {lat}, {long}")
+        #print(f"My coordinates: {lat}, {long}")
         
         curr_match = 100.00
         matched_city = ""
@@ -430,7 +632,7 @@ class Ui_MainWindow(object):
                 curr_match = calc_match
                 matched_city = str(city)
         
-        print(f"Matched city: {matched_city}")
+        #print(f"Matched city: {matched_city}")
         return matched_city
 
 
@@ -449,32 +651,20 @@ class Ui_MainWindow(object):
     def update_time(self):
         while self.update_time_started:
             # Set current time
-            self.timeLabel.setText(self.get_time())
+            curr_time = self.get_time()
+            self.timeLabel.setText(curr_time)
+            self.timeLabel_2.setText(curr_time)
             time.sleep(1)
-
-
-    def update_camera(self):
-        self.frame = self.vs.read()  
-        self.image = QtGui.QImage(self.frame.data, self.frame.shape[1], self.frame.shape[0], QtGui.QImage.Format_RGB888).rgbSwapped()
-        self.bgLabel.setPixmap(QPixmap.fromImage(self.image))
-        #cv2.destroyAllWindows()  
-        #self.vs.stop()
-        
-    def update_sensors(self):
-        total_distance = round(self.sensor.distance*100)
-        self.front_1a_Label.setPixmap(self.front_1a_ON if total_distance < self.dist_step_01 else self.front_1a_OFF)
-        self.front_1b_Label.setPixmap(self.front_1b_ON if total_distance < self.dist_step_02 else self.front_1b_OFF)
-        self.front_1c_Label.setPixmap(self.front_1c_ON if total_distance < self.dist_step_03 else self.front_1c_OFF)
-        self.front_1d_Label.setPixmap(self.front_1d_ON if total_distance < self.dist_step_04 else self.front_1d_OFF)  
-        print('Distance to nearest object is', total_distance, 'cm')
 
     def stop(self):
         print("Exiting...")
         self.update_location_started = False
         self.update_speed_started = False
         self.update_time_started = False
+        self.stop_media_player = True
 
 
         self.speed_thread.join()
         self.time_thread.join()
         self.location_thread.join()
+        self.media_player_thread.join()
